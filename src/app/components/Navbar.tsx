59f23d9e6b4fa217651d40e24f57675007b65342
Jose <josemaurette1234@gmail.com>
2025-06-15 17:32:44 -0400

    Removed Instagram icon from Navbar component for a cleaner design.

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -10,9 +10,6 @@ const Navbar = () => {
         <DockIcon onClick={() => window.open("https://github.com/JoseMaurette1", "_blank")}>
           <Github className="h-6 w-6 text-zinc-200 hover:text-zinc-50" />
         </DockIcon>
-        <DockIcon onClick={() => window.open("https://instagram.com/hose_04", "_blank")}>
-          <Instagram className="h-6 w-6 text-zinc-200 hover:text-zinc-50" />
-        </DockIcon>
         <DockIcon onClick={() => window.open("https://linkedin.com/in/josemaurette", "_blank")}>
           <Linkedin className="h-6 w-6 text-zinc-200 hover:text-zinc-50" />
         </DockIcon>
